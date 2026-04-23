142331a4847e549731314a1fcf1ae68ebaa3144a
MW Content Studio <mncontentstudio.web@gmail.com>
2026-04-23 21:11:37 +0530

    feat: add service worker with Workbox configuration and precaching logic

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -87,7 +87,7 @@ define(['./workbox-fa6cb374'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.am564hefiak"
+    "revision": "0.9afe0s8t074"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
